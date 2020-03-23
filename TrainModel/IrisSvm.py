60d5230f9799182546098b3e58a340f6d3c143a7
Sai Kalagotla <44039358+saikalagotla@users.noreply.github.com>
2020-03-23 13:36:57 -0400

    Another classifier
    Made a classifier using support vector machine

--- TrainModel/IrisSvm.py
+++ TrainModel/IrisSvm.py
@@ -1,11 +1,18 @@
+import sys
 import numpy as np
 from sklearn.svm import SVC
+sys.path.append("../readOrganizeData")
 import readData
 
-features = np.array(readData.readcsv(""))
-labels = np.array(readData.readcsv(""))
+features = np.array(readData.readcsv("IrisTrainingFeatures.csv"))
+labels = np.array(readData.readcsv("NumericalIrisTrainingLabels.csv"))
+labels = labels.ravel()
 
 clf = SVC(gamma='auto')
-clf.fit(x, y)
+clf.fit(features, labels)
 SVC(gamma='auto')
-print(clf.predict([[-0.8, -1]]))
+print(clf.predict([[4.5,2.3,1.3,0.3]]))
+
+testFeatures = np.array(readData.readcsv("IrisTestFeatures.csv"))
+testLabels = np.array(readData.readcsv("NumericalIrisTestLabels.csv"))
+print("Score = ", clf.score(testFeatures, testLabels)*100, "%")
--- TrainModel/TrainAndPredict.py
+++ TrainModel/TrainAndPredict.py
@@ -5,7 +5,7 @@ import readData
 from sklearn.naive_bayes import GaussianNB
 import matplotlib.pyplot as plt
 
-data = readData.readcsv("irisTrainingFeatureList.csv")
+data = readData.readcsv("irisTrainingFeatures.csv")
 labels = readData.readcsv("NumericalIrisTrainingLabels.csv")
 data = np.array(data).astype(np.float)
 labels = np.array(labels).astype(np.float)
@@ -22,5 +22,5 @@ labels = labels.ravel()
 
 print(clf.predict([[4.5,2.3,1.3,0.3]]))
 
-print("Score = ")
-print(clf.score(testFeatures, testLabels))
+print("Score = ", clf.score(testFeatures, testLabels)*100, "%")
+print()
